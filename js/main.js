d3f19d59f0ec70a221de5d4630e52c68efd8acf3
Master-Alpah <3030653596@qq.com>
2024-01-28 23:29:41 +0800

    code

--- js/main.js
+++ js/main.js
@@ -1,3 +1,14 @@
 $(document).ready(()=>{
     console.log("hello world")
+    $("#logIn").click(()=>{
+        console.log("login")
+        $("#login_dialog").modal("show")
+    });
+    $(".nav-link").hover(function(){
+        console.log(1)
+        $(this).next().css("display","block");
+    },function(){
+        console.log(2)
+        $(this).next().css("display","none");
+    });
 })
