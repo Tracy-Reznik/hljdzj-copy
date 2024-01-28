1576e74c9f338fd19443693d2da3bc269bea7160
Master-Alpah <3030653596@qq.com>
2024-01-29 02:28:27 +0800

    code

--- js/main.js
+++ js/main.js
@@ -11,4 +11,21 @@ $(document).ready(()=>{
         console.log(2)
         $(this).next().css("display","none");
     });
+    $(document).ready(function() {
+        var images = ["img/1eafd6f3db604c5396a384370ed32d40.jpg", "img/3bb", "url3"]; // 替换为你的图片URL
+        var index = 0;
+
+        function changeImage() {
+            var imageUrl = images[index];
+            $('.carousel-item').animate({
+                'margin-left': '-=' + $('.carousel-item').width()
+            }, 1000, function() {
+                $('.carousel-item').css('margin-left', 0);
+                $('.carousel-item').first().appendTo('#carousel');
+            });
+            index = (index + 1) % images.length; // 循环播放图片
+        }
+
+        setInterval(changeImage, 3000); // 每3秒更换一次图片
+    });
 })
